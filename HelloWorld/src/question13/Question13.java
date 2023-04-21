690a69299a4ba48e834c3b4e7dbc9cb202799569
shishime185 <ryoga1850202@gmail.com>
2023-04-22 00:15:34 +0900

    java課題１３が完了いたしました。

---
+++ HelloWorld/src/question13/Question13.java
@@ -0,0 +1,28 @@
+/*
+	int型の配列aを作成してください。
+	配列aに1～5の数字を格納してください。
+	繰り返し処理を使用し、
+	配列aの数字を順番に表示させてください。
+
+	出力結果：1
+			  2
+			  3
+			  4
+			  5
+*/
+
+package question13;
+
+class Question13 {
+
+	public static void main(String[] args) {
+//		1から5までの整数を格納
+		int[] a = {1, 2, 3, 4, 5};
+		System.out.println(a [0]);
+		System.out.println(a [1]);
+		System.out.println(a [2]);
+		System.out.println(a [3]);
+		System.out.println(a [4]);
+	}
+
+}
