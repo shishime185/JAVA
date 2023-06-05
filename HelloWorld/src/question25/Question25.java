32a138f25bda717d2d128b61a88a6cc1f1dfde14
shishime185 <ryoga1850202@gmail.com>
2023-06-06 07:12:01 +0900

    Java課題２５が完了いたしましたので、ご確認の程よろしくお願いいたします。

---
+++ HelloWorld/src/question25/Question25.java
@@ -0,0 +1,58 @@
+/*
+ 
+ 名前を入力したら下記がコンソールに出力されるように作ってください
+	条件：数値は毎回変わるように作ってください
+	 	 サブクラスを使用してください
+		 スーパークラスを使用してください
+		 getterとsetterを使用してください
+		 packageを2つ作ってメインと処理を分けてください
+		 命名する場合は規則にのっとってください
+		 コンストラクタを使用してください
+ 
+	こんにちは 「 名前 」 さん
+	ステータス
+	HP：849
+	MP：862
+	攻撃力：375
+	素早さ：937
+	防御力：24
+
+	さあ冒険に出かけよう！
+ 
+ */
+
+package question25;
+
+import java.util.Scanner;
+
+import question25_2.SubCharacter;
+
+public class Question25 {
+    public static void main(String[] args) {
+        // ユーザーに名前の入力を求める
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("名前を入力してください: ");
+        String name = scanner.nextLine();
+
+        // SubCharacterクラスのインスタンスを作成し、名前を設定
+        SubCharacter subCharacter = new SubCharacter(name);
+        
+        // ランダムステータス生成
+        subCharacter.generateRandomStatus();
+
+        // 挨拶とステータス出力
+        System.out.println("こんにちは、" + subCharacter.getName() + "さん");
+        System.out.println("ステータス");
+        System.out.println("HP: " + subCharacter.getHp());
+        System.out.println("MP: " + subCharacter.getMp());
+        System.out.println("攻撃力: " + subCharacter.getAttack());
+        System.out.println("素早さ: " + subCharacter.getAgility());
+        System.out.println("防御力: " + subCharacter.getDefense());
+
+        // 出力
+        System.out.println("さあ冒険に出かけよう！");
+
+        // Scannerを閉じる
+        scanner.close();
+    }
+}
