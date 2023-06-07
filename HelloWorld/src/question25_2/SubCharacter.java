464827ddddfeb85094b7c54a9a538e60512be33b
shishime185 <ryoga1850202@gmail.com>
2023-06-07 19:50:18 +0900

    追加

---
+++ HelloWorld/src/question25_2/SubCharacter.java
@@ -0,0 +1,10 @@
+package question25_2;
+
+//Question25_2クラスを継承したSubCharacterクラスの定義
+public class SubCharacter extends Question25_2 {
+
+    public SubCharacter(String name) {
+    	// スーパークラスのコンストラクタを呼び出して名前を設定
+        super(name); 
+    }
+}
