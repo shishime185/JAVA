e33e3016dc27c84c6af74d7e2f1d83d9e8b40ccd
shishime185 <ryoga1850202@gmail.com>
2023-06-18 23:44:57 +0900

    課題２２の修正が完了いたしましたので、ご確認の程よろしくお願いいたします。

--- curriculum1_22/src/curriculum1_22_2/Curriculum1_22_2.java
+++ curriculum1_22/src/curriculum1_22_2/Curriculum1_22_2.java
@@ -8,13 +8,13 @@ import java.util.Calendar;
 public class Curriculum1_22_2 {
 
 	// 1行目の変数
-	String greeting;
+	private String greeting;
 
 	// 2行目の変数
-	String sushi;
+	private String sushi;
 
 	// 3行目の変数
-	String cuisine;
+	private String cuisine;
 
 	// コンストラクターの定義
 	public Curriculum1_22_2() {
@@ -28,19 +28,19 @@ public class Curriculum1_22_2 {
 		// 3行目の初期値の設定
 		this.cuisine = "寿司は和食です";
 
-		// this.greetingを表示
+		// 出力
 		System.out.println(this.greeting);
 
-		// this.sushiを表示
+		// 出力
 		System.out.println(this.sushi);
 
-		// this.cuisineを表示
+		// 出力
 		System.out.println(this.cuisine);
 
 		// カレンダーオブジェクトの生成
 		Calendar calendar = Calendar.getInstance();
 
-		// SimpleDateFormatクラスでフォーマットパターンを設定する
+		// フォーマットパターンを設定する
 		SimpleDateFormat sdf = new SimpleDateFormat("今の現在日時はyyyy/MM/dd H:m:s");
 
 		// 出力
