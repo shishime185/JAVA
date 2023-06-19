4499541806f4b55489d8210eb36e10545b716038
shishime185 <ryoga1850202@gmail.com>
2023-06-19 19:56:16 +0900

    JavaカリキュラムBのQ 1~3の修正が完了いたしましたので、ご確認の程よろしくお願いいたします。

--- HelloWorld/src/curriculum_B/Qes1_3.java
+++ HelloWorld/src/curriculum_B/Qes1_3.java
@@ -8,35 +8,38 @@ class Qes1_3 {
     private static final String[] HANDS = {"グー", "チョキ", "パー"};
 
     public static void main(String[] args) {
+    	
         Scanner scanner = new Scanner(System.in);
         String userName = "";
 
         // ユーザー名入力
         boolean validName = false;
         while (!validName) {
-            userName = scanner.nextLine();
+        userName = scanner.nextLine();
 
             // 入力ミスの時
             if (userName == null || userName.length() <= 0) {
                 System.out.println("名前を入力してください");
+                
             } else if (userName.length() > 10) {
                 System.out.println("名前を10文字以内にしてください");
+                
             } else if (!userName.matches("[a-zA-Z0-9]+")) {
                 System.out.println("半角英数字のみで名前を入力してください");
+                
             } else {
-                System.out.println("ユーザー名「" + userName + "」を登録しました\n");
+                System.out.printf("ユーザー名「%s」を登録しました\n\n", userName);
                 startRockPaperScissors(userName, scanner);
                 validName = true;
             }
-            
-  
         }
-        
+
         // Scannerオブジェクトを閉じる
         scanner.close();
     }
 
     private static void startRockPaperScissors(String userName, Scanner scanner) {
+    	
         // 総回数の変数を初期化
         int totalRounds = 0;
         Random random = new Random();
@@ -46,45 +49,51 @@ class Qes1_3 {
         do {
             // ユーザーの手をランダムに選択
             int userHand = random.nextInt(3);
-            System.out.println(userName + "の手は「" + HANDS[userHand] + "」");
+            System.out.printf("%sの手は「%s」\n", userName, HANDS[userHand]);
 
             // コンピュータの手をランダムに選択
             int computerHand = random.nextInt(3);
-            System.out.println("相手の手は「" + HANDS[computerHand] + "」\n");
+            System.out.printf("相手の手は「%s」\n\n", HANDS[computerHand]);
+
             // 総回数をインクリメント
             totalRounds++;
-
 
             // 引き分けの場合
             if (userHand == computerHand) {
                 System.out.println("DRAW あいこ もう一回しましょう！\n");
-
-            } else if ((userHand == 0 && computerHand == 1) || (userHand == 1 && computerHand == 2)
-                    || (userHand == 2 && computerHand == 0)) {
                 
+            } else if ((userHand == 0 && computerHand == 1) 
+            		|| (userHand == 1 && computerHand == 2)
+                    || (userHand == 2 && computerHand == 0)) {
+            	
                 // ユーザーが勝ちの場合
                 System.out.println("やるやん。");
                 System.out.println("次は俺にリベンジさせて\n");
+                
                 // ゲーム終了フラグをセット
                 continueGame = false;
                 
             } else {
                 // ユーザーの負けの場合
                 System.out.println("俺の勝ち！");
+                
                 if (userHand == 0 && computerHand == 2) {
-                    System.out.println("なんで負けたか、明日まで考えといてください。\nそしたら何かが見えてくるはずです\n");
-                } else if (userHand == 1 && computerHand == 0) {
-                    System.out.println("負けは次につながるチャンスです！\nネバーギブアップ！\n");
-                } else {
-                    System.out.println("たかがじゃんけん、そう思ってないですか？\nそれやったら次も、俺が勝ちますよ\n");
+                    System.out.println("なんで負けたか、明日まで考えといてください。");
+                    System.out.println("そしたら何かが見えてくるはずです\n");
                     
+                } else if (userHand == 1 && computerHand == 0) {
+                    System.out.println("負けは次につながるチャンスです！");
+                    System.out.println("ネバーギブアップ！\n");
+                    
+                } else {
+                    System.out.println("たかがじゃんけん、そう思ってないですか？");
+                    System.out.println("それやったら次も、俺が勝ちますよ\n");
                 }
-
             }
-            // ゲーム継続フラグが true の場合は繰り返し
+            // ゲーム継続フラグがtrueの場合は繰り返し
         } while (continueGame);
 
         // 出力
-        System.out.println("勝つまでにかかった合計回数は" + totalRounds + "回です");
+        System.out.printf("勝つまでにかかった合計回数は%d回です\n", totalRounds);
     }
 }
