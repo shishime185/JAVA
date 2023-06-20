ceee17ba6048a7715e4bec9ea5dec3abf1f04240
shishime185 <ryoga1850202@gmail.com>
2023-06-20 18:49:04 +0900

    カリキュラムBのQ1~3の修正が完了いたしましたので、ご確認の程よろしくお願いいたします。

--- HelloWorld/src/curriculum_B/Qes1_3.java
+++ HelloWorld/src/curriculum_B/Qes1_3.java
@@ -8,14 +8,13 @@ class Qes1_3 {
     private static final String[] HANDS = {"グー", "チョキ", "パー"};
 
     public static void main(String[] args) {
-    	
         Scanner scanner = new Scanner(System.in);
         String userName = "";
 
         // ユーザー名入力
         boolean validName = false;
         while (!validName) {
-        userName = scanner.nextLine();
+            userName = scanner.nextLine();
 
             // 入力ミスの時
             if (userName == null || userName.length() <= 0) {
@@ -61,36 +60,33 @@ class Qes1_3 {
             // 引き分けの場合
             if (userHand == computerHand) {
                 System.out.println("DRAW あいこ もう一回しましょう！\n");
-                
-            } else if ((userHand == 0 && computerHand == 1) 
-            		|| (userHand == 1 && computerHand == 2)
-                    || (userHand == 2 && computerHand == 0)) {
-            	
-                // ユーザーが勝ちの場合
-                System.out.println("やるやん。");
-                System.out.println("次は俺にリベンジさせて\n");
-                
-                // ゲーム終了フラグをセット
-                continueGame = false;
-                
             } else {
-                // ユーザーの負けの場合
-                System.out.println("俺の勝ち！");
-                
-                if (userHand == 0 && computerHand == 2) {
-                    System.out.println("なんで負けたか、明日まで考えといてください。");
-                    System.out.println("そしたら何かが見えてくるはずです\n");
-                    
-                } else if (userHand == 1 && computerHand == 0) {
-                    System.out.println("負けは次につながるチャンスです！");
-                    System.out.println("ネバーギブアップ！\n");
+                // 勝利条件を判定
+                boolean isWin = (userHand == 0 && computerHand == 1) ||
+                                (userHand == 1 && computerHand == 2) ||
+                                (userHand == 2 && computerHand == 0);
+
+                if (isWin) {
+                    System.out.println("やるやん。");
+                    System.out.println("次は俺にリベンジさせて\n");
+                    continueGame = false;
                     
                 } else {
-                    System.out.println("たかがじゃんけん、そう思ってないですか？");
-                    System.out.println("それやったら次も、俺が勝ちますよ\n");
+                    System.out.println("俺の勝ち！");
+                    if (userHand == 0 && computerHand == 2) {
+                        System.out.println("なんで負けたか、明日まで考えといてください。");
+                        System.out.println("そしたら何かが見えてくるはずです\n");
+                        
+                    } else if (userHand == 1 && computerHand == 0) {
+                        System.out.println("負けは次につながるチャンスです！");
+                        System.out.println("ネバーギブアップ！\n");
+                        
+                    } else {
+                        System.out.println("たかがじゃんけん、そう思ってないですか？");
+                        System.out.println("それやったら次も、俺が勝ちますよ\n");
+                    }
                 }
             }
-            // ゲーム継続フラグがtrueの場合は繰り返し
         } while (continueGame);
 
         // 出力
